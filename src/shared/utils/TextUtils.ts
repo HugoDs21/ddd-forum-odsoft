cf7ad62a913eb464d0d664e6c03f8900ec64afe0
HugoDs21 <hugodias.hmmd@gmail.com>
2023-10-11 18:22:18 +0100

    Gradle task para gerar docs

--- src/shared/utils/TextUtils.ts
+++ src/shared/utils/TextUtils.ts
@@ -5,16 +5,35 @@ import DOMPurify from 'dompurify'
 const { window } = new JSDOM('<!DOCTYPE html>')
 const domPurify = DOMPurify(window)
 
+/**
+ * TextUtils
+ * @class TextUtils
+ */
 export class TextUtils {
 
+  /**
+   * Sanitize text to prevent XSS attacks
+   * @param unsafeText
+   * @returns {string}
+   */
   public static sanitize (unsafeText: string): string {
     return domPurify.sanitize(unsafeText);
   }
 
+  /**
+   * Remove all HTML tags from text
+   * @param text
+   * @returns {string}
+   */ 
   public static validateWebURL (url: string): boolean {
     return validator.isURL(url);
   }
 
+  /**
+   * Validate email address format
+   * @param text String
+   * @returns {string}
+   */
   public static validateEmailAddress (email: string) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
