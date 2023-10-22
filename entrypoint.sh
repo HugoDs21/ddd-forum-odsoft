31007200f49543fb39658558f6a43c3a151e10ac
HugoDs21 <hugodias.hmmd@gmail.com>
2023-10-22 19:12:08 +0100

    Fix notation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-echo "Waitin for mysql to start..."
+echo Waitin for mysql to start...
 
 while ! nc -z ddd_forum_mysql 3306; do
   sleep 10
 done
 
-echo "MySQL started"
+echo MySQL started
 
 while ! nc -z ddd_forum_redis 6379; do
   sleep 10
 done
  
-echo "REDIS started"
+echo REDIS started
 
 npm run db:create:dev
 npm run migrate:dev
